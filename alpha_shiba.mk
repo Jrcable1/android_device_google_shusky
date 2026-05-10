b5aebdb0a882d5bb940930a5c1ae5bf40959d21f
jrcable1 <jrcable1@gmail.com>
2026-05-10 18:44:22 -0400

    Device Bring up for Alpha

--- alpha_shiba.mk
+++ alpha_shiba.mk
@@ -5,29 +5,29 @@
 #
 
 # Inherit some common stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/alpha/config/common_full_phone.mk)
 
 # Inherit device configuration
-DEVICE_CODENAME := husky
+DEVICE_CODENAME := shiba
 DEVICE_PATH := device/google/shusky
-VENDOR_PATH := vendor/google/husky
+VENDOR_PATH := vendor/google/shiba
 $(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
 
 # setup dalvik vm configs.
-$(call inherit-product, frameworks/native/build/phone-xhdpi-12288-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-8192-dalvik-heap.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
-PRODUCT_MODEL := Pixel 8 Pro
-PRODUCT_NAME := lineage_$(DEVICE_CODENAME)
+PRODUCT_MODEL := Pixel 8
+PRODUCT_NAME := alpha_$(DEVICE_CODENAME)
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 2992
-TARGET_SCREEN_WIDTH := 1344
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="husky-user 16 BP4A.260205.001 14624666 release-keys" \
-    BuildFingerprint=google/husky/husky:16/BP4A.260205.001/14624666:user/release-keys \
+    BuildDesc="shiba-user 16 BP4A.260205.001 14624666 release-keys" \
+    BuildFingerprint=google/shiba/shiba:16/BP4A.260205.001/14624666:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
