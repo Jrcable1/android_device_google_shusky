35c7a17aaf8f87174bc7428c449864fbbeaa93c7
jrcable1 <jrcable1@gmail.com>
2026-05-10 18:49:25 -0400

    Add Alpha and Device Flags

--- alpha_shiba.mk
+++ alpha_shiba.mk
@@ -27,6 +27,44 @@ PRODUCT_NAME := alpha_$(DEVICE_CODENAME)
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Device config
+TARGET_HAS_UDFPS := true
+TARGET_SUPPORTS_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Build config
+
+# append time of day to zip
+ALPHA_VERSION_APPEND_TIME_OF_DAY := false
+
+# TARGET_BUILD_PACKAGE options:
+# 1 - vanilla (default)
+# 2 - microg
+# 3 - gapps
+TARGET_BUILD_PACKAGE := 3
+
+# (valid only for GAPPS builds)
+TARGET_INCLUDE_GOOGLE_COMMS := true
+TARGET_INCLUDE_PIXEL_LAUNCHER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+WITH_PIXEL_OVERLAYS := true
+
+# Debugging
+TARGET_INCLUDE_MATLOG := false
+WITH_ADB_INSECURE := false
+
+# Extras
+TARGET_INCLUDE_SIMPLE_TUNE := true
+
+# Maintainer
+ALPHA_BUILD_TYPE := Official
+ALPHA_MAINTAINER := Jrcable2
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="shiba-user 16 BP4A.260205.001 14624666 release-keys" \
     BuildFingerprint=google/shiba/shiba:16/BP4A.260205.001/14624666:user/release-keys \
